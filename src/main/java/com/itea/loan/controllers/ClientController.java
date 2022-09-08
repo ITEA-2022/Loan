22050df11fab4697d4b9ed4370564b401de3a100
Dmytro Zakharchenko <zaharr3@gmail.com>
2022-09-08 18:48:08 +0300

    Edit ClientController

--- src/main/java/com/itea/loan/controllers/ClientController.java
+++ src/main/java/com/itea/loan/controllers/ClientController.java
@@ -25,16 +25,6 @@ public class ClientController {
         Date date= new Date();
         DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG);
         String formattedDate= dateFormat.format(date);
-//        private String clientName;
-//        private int age;
-//        private String address;
-//        private String sex;
-//        private Date birth_day;
-//        private String ipn;
-//        private String passport;
-        //clientRepository.save(new Clients("name", 25, "address", "male", "20020101","1111111","passp"));
-        //Clients client = new Clients("name");
-        //clientRepository.save(client);
         clientRepository.save(new Clients(clientdto.getClientName(),clientdto.getAge(),clientdto.getAddress(),clientdto.getSex(),clientdto.getBirth_day(),clientdto.getIpn(),clientdto.getPassport()));
         return ResponseEntity.ok(clientdto);
     }
